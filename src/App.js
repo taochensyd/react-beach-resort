1f44727be1b5b518eabd84369c334fb018278098
Tao Chen <54517105+taochensyd@users.noreply.github.com>
2020-10-12 23:22:41 +1100

    setup basic pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
 
 function App() {
   return (
     <>
-      app.js
+      <Home></Home>
+      <Rooms></Rooms>
+      <SingleRoom></SingleRoom>
+      <Error></Error>
     </>
   );
 }
