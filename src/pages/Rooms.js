8d7995c3a643d2e034d2e5c2b073a72b11af7e95
Tao Chen <54517105+taochensyd@users.noreply.github.com>
2020-10-13 15:47:07 +1100

    add banner to each pages

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,5 +1,18 @@
 import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
-export default function Rooms() {
-  return <div>Room.js</div>;
-}
+const Rooms = () => {
+  return (
+    <Hero hero="roomsHero">
+      <Banner title="our rooms">
+        <Link to="/" className="btn-primary">
+          return home
+        </Link>
+      </Banner>
+    </Hero>
+  );
+};
+
+export default Rooms;
