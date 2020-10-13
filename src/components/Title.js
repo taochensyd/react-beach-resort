cc9c80ed57002e6c5bafd728cda4991059c808f9
Tao Chen <54517105+taochensyd@users.noreply.github.com>
2020-10-13 16:08:27 +1100

    fix typo error in title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Title({ title }) {
   return (
-    <div classsName='section-title'>
+    <div className='section-title'>
       <h4>{title}</h4>
       <div></div>
     </div>
