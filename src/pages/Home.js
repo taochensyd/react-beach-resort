c110a91e9608766be4ab685c0a41c0b5e863c7c6
Tao Chen <54517105+taochensyd@users.noreply.github.com>
2020-10-13 16:07:10 +1100

    change name for title.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,20 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Services from "../components/Services";
 
 export default function Home() {
   return (
-    <Hero>
-      <Banner
-        title="luxurious rooms"
-        subtitle="deluxe room starting at $299">
+    <React.Fragment>
+      <Hero>
+        <Banner title="luxurious rooms" subtitle="deluxe room starting at $299">
           <Link to="/rooms" className="btn-primary">
             our rooms
           </Link>
         </Banner>
-    </Hero>
+      </Hero>
+      <Services></Services>
+    </React.Fragment>
   );
 }
