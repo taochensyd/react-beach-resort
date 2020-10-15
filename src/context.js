51bfb2b04635947f81a79c843d9d95e0ac5b8202
Tao Chen <54517105+taochensyd@users.noreply.github.com>
2020-10-15 20:43:38 +1100

    add getRoom(slug), using find()

--- src/context.js
+++ src/context.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import items from "./data";
+import Room from "./components/Room";
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
@@ -31,9 +32,15 @@ class RoomProvider extends Component {
     return tempItems;
   }
 
+  getRoom = (slug) => {
+    let tempRooms = [...this.state.Room];
+    const room = tempRooms.find((room) => room.slug === slug);
+    return room;
+  };
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
     );
