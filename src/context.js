bdb3386183daa87174c5be55ca89b2addce4c1ae
Tao Chen <54517105+taochensyd@users.noreply.github.com>
2020-10-15 20:02:32 +1100

    add loading section when waiting for data

--- src/context.js
+++ src/context.js
@@ -12,6 +12,13 @@ class RoomProvider extends Component {
 
   componentDidMount() {
     let rooms = this.formatData(items);
+    let featuredRooms = rooms.filter((room) => room.featured === true);
+    this.setState({
+      rooms,
+      featuredRooms,
+      sortedRooms: rooms,
+      loading: false,
+    });
   }
 
   formatData(items) {
